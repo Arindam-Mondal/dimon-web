be506a9d9588f44693a6264dfbdf3a31b356ac93
Arindam <ariron123@gmail.com>
2020-01-12 15:33:29 +0530

    Updates

---
+++ precache-manifest.ae372c396065fad7f359e2cd224a910b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7c4008fed99d182cbaebe3cc324602",
+    "url": "/dimon-web/index.html"
+  },
+  {
+    "revision": "784a3f3ac7db26f68871",
+    "url": "/dimon-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4b781f44e3786683355",
+    "url": "/dimon-web/static/js/2.9eeae818.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dimon-web/static/js/2.9eeae818.chunk.js.LICENSE"
+  },
+  {
+    "revision": "784a3f3ac7db26f68871",
+    "url": "/dimon-web/static/js/main.be5fc251.chunk.js"
+  },
+  {
+    "revision": "18122dfba11fe26787d7",
+    "url": "/dimon-web/static/js/runtime-main.88800d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dimon-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
